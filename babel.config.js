6deefc3b382cc6d5b76eef7532e7a221bb045694
TuanLDT <thanhtuan.pfiev@gmail.com>
2018-12-06 11:29:24 +0700

    chore: update

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,13 @@ const presets = [
 ];
 
 const plugins = [
-	'@babel/plugin-proposal-class-properties'
+	'@babel/plugin-proposal-class-properties',
+  ["@babel/plugin-transform-runtime", {
+    "corejs": false,
+    "helpers": true,
+    "regenerator": true,
+    "useESModules": false
+  }]
 ];
 
 module.exports = { presets, plugins };
